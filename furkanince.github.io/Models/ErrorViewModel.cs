d3f254f719aafe601822d3a18096f06f1791a2b2
Furkan Ince <furkan.ince@tepeguvenlik.com.tr>
2022-04-05 16:51:39 +0300

    Proje dosyası ekle.

---
+++ furkanince.github.io/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace furkanince.github.io.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
